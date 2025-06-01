a814a4a548f1afc33a605231ba2f56ade132a10c
arifur-rahman1 <aarifurrahman93@gmail.com>
2025-06-01 13:38:06 +0600

    header and sidebar done

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+          
+          <Sidebar />
+        </div>
+      </main>
+    </>
+  );
+}
+
+export default App;
