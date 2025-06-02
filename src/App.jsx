519c741503abc3a8f14d5e4a96c08b97ed6c120e
arifur-rahman1 <aarifurrahman93@gmail.com>
2025-06-02 14:55:07 +0600

    dinamically data added

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
 import "./App.css";
+import MovieList from "./cine/MovieList";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
+
 
 function App() {
   return (
     <>
       <Header />
       <main>
-        <div class="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
+        <div className="container grid lg:grid-cols-[218px_1fr] gap-[3.5rem]">
           
           <Sidebar />
+          <MovieList/>
         </div>
       </main>
     </>
